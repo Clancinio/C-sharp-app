4582c7dd8bd5491f78f20f10edbf7f236e084c87
DeanC <dean.clancy91@gmail.com>
2020-02-10 18:42:17 -0800

    Add project files.

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string schoolName;
+        string schoolAddress;
+        string schoolCity;
+        string schoolState;
+        string schoolZip;
+        string schoolNumber;
+    }
+}
