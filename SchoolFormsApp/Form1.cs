c44e911dba8fe3a7f7a019867f3c184ad2f88a1d
DeanC <dean.clancy91@gmail.com>
2020-02-11 15:22:13 -0800

    Add labels, text boxes, and button to form

--- SchoolFormsApp/Form1.cs
+++ SchoolFormsApp/Form1.cs
@@ -17,5 +17,19 @@ namespace SchoolFormsApp
             InitializeComponent();
         }
 
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
